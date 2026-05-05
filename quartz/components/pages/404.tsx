e1485f0324b378e284cd2215d56d6db570ccc673
EnrikaB <hfbpriv@gmail.com>
2026-05-05 02:36:15 +0200

    Quartz sync: May 5, 2026, 2:36 AM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -2,22 +2,19 @@ import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"  
   
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {  
-  // Generate random Pokemon ID (1-1010 for current Pokemon count)  
-  const pokemonId = Math.floor(Math.random() * 1010) + 1  
-    
   return (  
     <article class="popover-hint" style="text-align: center; padding: 2rem;">  
       <h1>404</h1>  
       <p>This page seems to have wandered off into the tall grass!</p>  
         
       <div id="pokemon-container" style="margin: 2rem 0;">  
-        <img   
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`}  
+        <img     
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"  
           alt="Pokemon"  
           style="width: 96px; height: 96px; image-rendering: pixelated;"  
         />  
         <p style="margin-top: 0.5rem; font-family: monospace;">  
-          #{pokemonId}  
+          #25  
         </p>  
       </div>  
         
@@ -28,22 +25,40 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
       <script  
         dangerouslySetInnerHTML={{  
           __html: `  
-            // Show a new Pokemon on each navigation  
-            document.addEventListener('nav', () => {  
+            function showRandomPokemon() {  
               const container = document.getElementById('pokemon-container');  
               if (container) {  
                 const newId = Math.floor(Math.random() * 1010) + 1;  
                 container.innerHTML = \`  
-                  <img   
-                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\${newId}.png"  
-                    alt="Pokemon"  
-                    style="width: 96px; height: 96px; image-rendering: pixelated;"  
+                  <img     
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\${newId}.png"    
+                    alt="Pokemon"    
+                    style="width: 96px; height: 96px; image-rendering: pixelated;"    
                   />  
-                  <p style="margin-top: 0.5rem; font-family: monospace;">  
-                    #\${newId}  
+                  <p style="margin-top: 0.5rem; font-family: monospace;">    
+                    #\${newId}    
                   </p>  
                 \`;  
               }  
+            }  
+              
+            // Wait for DOM to be ready  
+            if (document.readyState === 'loading') {  
+              document.addEventListener('DOMContentLoaded', showRandomPokemon);  
+            } else {  
+              showRandomPokemon();  
+            }  
+              
+            // Show new Pokemon on each navigation  
+            document.addEventListener('nav', showRandomPokemon);  
+              
+            // Also show new Pokemon when DOM is rendered (for popovers)  
+            document.addEventListener('render', showRandomPokemon);  
+              
+            // Cleanup on navigation  
+            window.addCleanup(() => {  
+              document.removeEventListener('nav', showRandomPokemon);  
+              document.removeEventListener('render', showRandomPokemon);  
             });  
           `,  
         }}  
