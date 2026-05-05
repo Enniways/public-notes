3c198003c3036e5c9ccd878c001404b546660283
EnrikaB <hfbpriv@gmail.com>
2026-05-05 04:03:59 +0200

    Quartz sync: May 5, 2026, 4:03 AM

--- quartz.ts
+++ quartz.ts
@@ -1,20 +1,5 @@
-import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"  
-import * as ExternalPlugin from "./.quartz/plugins"  
-import { contentImage } from "./quartz/components/ContentImage"  
-  
-const config = await loadQuartzConfig({  
-  plugins: {  
-    emitters: [  
-      ExternalPlugin.CustomOgImages({  
-        colorScheme: "darkMode",  
-        width: 1200,  
-        height: 630,  
-        excludeRoot: false,  
-        imageStructure: contentImage, // Your custom component  
-      }),  
-    ],  
-  },  
-})  
-  
-export default config  
+import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+
+const config = await loadQuartzConfig()
+export default config
 export const layout = await loadQuartzLayout()
